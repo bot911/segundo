750d267875d970df9a34fb78bc49e029794a4346
bot911 <80369809+bot911@users.noreply.github.com>
2021-03-15 17:09:15 -0300

    Create index.js
    evento click no close

---
+++ index.js
@@ -0,0 +1,8 @@
+const $btnClose = document.querySelector('.d-inline-block.mr-2 button')
+
+$btnClose.addEventListener("click", () => {  
+    console.log('Hello word');
+    
+    window.location.pathname;
+
+})
